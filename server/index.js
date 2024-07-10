519b2acb77941af664835af90900ceaf3ea78c08
Nitish kumar <nitishkumar2809@gmail.com>
2024-07-10 20:10:32 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -8,14 +8,6 @@ const app = require('./app');
 const server = http.createServer(app);
 console.log(__dirname);
 
-// Use the client app, resolving the path correctly
-app.use(express.static(path.join(__dirname, '/client/myapp/build')));
-
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname, '/client/myapp/build/index.html'))
-})
-
-
 const io = socketIo(server, {
   cors: {
     origin: '*',
